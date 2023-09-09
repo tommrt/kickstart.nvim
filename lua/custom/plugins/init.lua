5f57a8c1aeed56af0c562617fea37746e15e2fac
tommrt <79672291+tommrt@users.noreply.github.com>
2023-09-10 07:24:23 +1000

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,12 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  {
-        "max397574/better-escape.nvim",
-        event = "InsertEnter",
-        config = function()
-            require("better_escape").setup()
-        end,
-    },
-}
+return { }
