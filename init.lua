3488485ddffe6e2db9e02e4bdc51dd8db7267c82
tommrt <79672291+tommrt@users.noreply.github.com>
2023-09-10 07:21:18 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -270,7 +270,7 @@ vim.o.termguicolors = true
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set({ 'i' }, 'jj', '<Esc>', { silent = true })
+vim.keymap.set({ 'n' }, ';', ':', { desc = "enter command mode" })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
